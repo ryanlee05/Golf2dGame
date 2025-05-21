88bc3e1bd06931f9917292b4c41909d8631126ea
ryanlee05 <ryanworld2005@icloud.com>
2025-05-20 21:12:01 -0400

    changed module-info.java and updated ryan.java

--- src/golf/ryan.java
+++ src/golf/ryan.java
@@ -2,6 +2,7 @@ package golf;
 
 import javax.swing.JFrame;
 
+
 public class ryan {
 
 	/**
@@ -12,9 +13,11 @@ public class ryan {
 	public static void main(String args[]) {
 		
 		//establish a new window
-		JFrame window = new JFrame();
-		
-		
+		JFrame window = new JFrame("Arise to Golf");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setVisible(true);
+		window.setLocationRelativeTo(null);
 		
 	}
 
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Golf2dGame {
+	requires java.desktop;
 }
