22f970be602cd8983d9400ec2c112e0b59f5ff5a
ryanlee05 <ryanworld2005@icloud.com>
2025-05-26 18:30:06 -0400

    Fixed club pop up screen to only when you collide with ball

--- src/Object/Clubs.java
+++ src/Object/Clubs.java
@@ -7,72 +7,70 @@ import golf.KeyHandler;
 import golf.gamePanel;
 
 public class Clubs {
-    gamePanel gp;
-    KeyHandler keyH;
-    BufferedImage[] clubImages = new BufferedImage[4];
-    BufferedImage[] hImages = new BufferedImage[4];
-    
-    int currentIndex = 0;
-    int x, y;
-    boolean qPressedLastFrame = false;
-    boolean ePressedLastFrame = false;
-    
+	gamePanel gp;
+	KeyHandler keyH;
+	BufferedImage[] clubImages = new BufferedImage[4];
+	BufferedImage[] hImages = new BufferedImage[4];
 
+	int currentIndex = 0;
+	int x, y;
+	boolean qPressedLastFrame = false;
+	boolean ePressedLastFrame = false;
 
-public Clubs(gamePanel gp, KeyHandler keyH) {
-    this.gp = gp;
-    this.keyH = keyH;
-    this.x = 15 + gp.tileSize/2;
-    this.y = gp.screenHeight - gp.tileSize;
-    try {
-        clubImages[0] = ImageIO.read(getClass().getResourceAsStream("/clubs/Driver.png"));
-        clubImages[1] = ImageIO.read(getClass().getResourceAsStream("/clubs/Iron.png"));
-        clubImages[2] = ImageIO.read(getClass().getResourceAsStream("/clubs/Wedge.png"));
-        clubImages[3] = ImageIO.read(getClass().getResourceAsStream("/clubs/Putter.png"));
-        hImages[0] = ImageIO.read(getClass().getResourceAsStream("/clubs/Driver_h.png"));
-        hImages[1] = ImageIO.read(getClass().getResourceAsStream("/clubs/Iron_h.png"));
-        hImages[2] = ImageIO.read(getClass().getResourceAsStream("/clubs/Wedge_h.png"));
-        hImages[3] = ImageIO.read(getClass().getResourceAsStream("/clubs/Putter_h.png"));
-    }
-    catch(Exception e) {
-        e.printStackTrace();
-    }
-    
-   
-    
-    
-}
+	public Clubs(gamePanel gp, KeyHandler keyH) {
+		this.gp = gp;
+		this.keyH = keyH;
+		this.x = 15 + gp.tileSize / 2;
+		this.y = gp.screenHeight - gp.tileSize;
+		try {
+			clubImages[0] = ImageIO.read(getClass().getResourceAsStream("/clubs/Driver.png"));
+			clubImages[1] = ImageIO.read(getClass().getResourceAsStream("/clubs/Iron.png"));
+			clubImages[2] = ImageIO.read(getClass().getResourceAsStream("/clubs/Wedge.png"));
+			clubImages[3] = ImageIO.read(getClass().getResourceAsStream("/clubs/Putter.png"));
+			hImages[0] = ImageIO.read(getClass().getResourceAsStream("/clubs/Driver_h.png"));
+			hImages[1] = ImageIO.read(getClass().getResourceAsStream("/clubs/Iron_h.png"));
+			hImages[2] = ImageIO.read(getClass().getResourceAsStream("/clubs/Wedge_h.png"));
+			hImages[3] = ImageIO.read(getClass().getResourceAsStream("/clubs/Putter_h.png"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-public void update() {
-    if (keyH.qPressed && !qPressedLastFrame) {
-    	gp.playSoundEffect(4);
-        currentIndex = (currentIndex - 1 + clubImages.length) % clubImages.length;
-    }
+	}
 
-    if (keyH.ePressed && !ePressedLastFrame) {
-    	gp.playSoundEffect(3);
-        currentIndex = (currentIndex + 1) % clubImages.length;
-    }
+	public void update() {
+		if (gp.golfBall.playerReady) {
+			if (keyH.qPressed && !qPressedLastFrame) {
+				gp.playSoundEffect(4);
+				currentIndex = (currentIndex - 1 + clubImages.length) % clubImages.length;
+			}
 
-    qPressedLastFrame = keyH.qPressed;
-    ePressedLastFrame = keyH.ePressed;
-}
+			if (keyH.ePressed && !ePressedLastFrame) {
+				gp.playSoundEffect(3);
+				currentIndex = (currentIndex + 1) % clubImages.length;
+			}
 
-public void draw(Graphics2D g2) {
-    int size = gp.tileSize;
-    int smallSize = size / 2 + 10;
-    
+			qPressedLastFrame = keyH.qPressed;
+			ePressedLastFrame = keyH.ePressed;
+		}
 
-    int prevIndex = (currentIndex - 1 + clubImages.length) % clubImages.length;
-    int nextIndex = (currentIndex + 1) % clubImages.length;
+	}
 
-   
-    g2.drawImage(clubImages[prevIndex], x - size/2 - 10, y + gp.tileSize/4 - 50, smallSize + 50, smallSize + 50, null);
+	public void draw(Graphics2D g2) {
+		int size = gp.tileSize;
+		int smallSize = size / 2 + 10;
 
-  
-    g2.drawImage(hImages[currentIndex], x + 50, y - 50, size + 50, size + 50, null);
+		int prevIndex = (currentIndex - 1 + clubImages.length) % clubImages.length;
+		int nextIndex = (currentIndex + 1) % clubImages.length;
 
-  
-    g2.drawImage(clubImages[nextIndex], x + size + 100, y + gp.tileSize/4 - 50, smallSize + 50, smallSize + 50, null);
-}
+		if (gp.golfBall.playerReady) {
+			g2.drawImage(clubImages[prevIndex], x - size / 2 - 10, y + gp.tileSize / 4 - 50, smallSize + 50,
+					smallSize + 50, null);
+
+			g2.drawImage(hImages[currentIndex], x + 50, y - 50, size + 50, size + 50, null);
+
+			g2.drawImage(clubImages[nextIndex], x + size + 100, y + gp.tileSize / 4 - 50, smallSize + 50,
+					smallSize + 50, null);
+		}
+
+	}
 }
